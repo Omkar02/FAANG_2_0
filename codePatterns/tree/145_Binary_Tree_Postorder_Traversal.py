d3219acf72f0dd14606636aaca6d975fa75ff3dc
Omkar02 <omkjoshi.dev.07@gmail.com>
2024-05-26 22:51:53 +0530

    GOOGLE DAY-15

---
+++ codePatterns/tree/145_Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,30 @@
+"""
+* Example 1:
+    Input: root = [1,null,2,3]
+    Output: [1,3,2]
+* Example 2:
+    Input: root = []
+    Output: []
+* Example 3:
+    Input: root = [1]
+    Output: [1]
+"""
+from DataStructure import readyTree, BST, bNode
+
+
+def inorderTraversal(root: bNode) -> list[int]:
+    res = []
+
+    def _traverse(node: bNode) -> None:
+        if not node:
+            return
+
+        _traverse(node.left)
+        _traverse(node.right)
+        res.append(node.val)
+    _traverse(root)
+    return res
+
+
+tree: BST = readyTree.printTree()
+print(inorderTraversal(readyTree.getHead()))
---
+++ codePatterns/tree/94_Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,29 @@
+"""
+* Example 1:
+    Input: root = [1,null,2,3]
+    Output: [1,3,2]
+* Example 2:
+    Input: root = []
+    Output: []
+* Example 3:
+    Input: root = [1]
+    Output: [1]
+"""
+from DataStructure import readyTree, BST, bNode
+
+
+def inorderTraversal(root: bNode) -> list[int]:
+    res = []
+
+    def _traverse(node: bNode) -> None:
+        if not node:
+            return
+        _traverse(node.left)
+        res.append(node.val)
+        _traverse(node.right)
+    _traverse(root)
+    return res
+
+
+tree: BST = readyTree.printTree()
+print(inorderTraversal(readyTree.getHead()))
